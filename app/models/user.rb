2c7a328e6bed495cfc12d5e76123465f66c1e3f9
Miwka-prog <shelestyukm@gmail.com>
2021-08-27 13:11:43 +0300

    Add annotate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  birth_date             :date
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  gender                 :integer          default(0), not null
+#  last_name              :string
+#  mobile                 :string           default("")
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Allowlist
 
@@ -6,6 +34,14 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :allowlisted_jwts, dependent: :destroy
+
+  validates :email, format: {
+    with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|
+ (\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/,
+    message: 'Invalid email'
+  }
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, on: :create
 
   def for_display
     {
